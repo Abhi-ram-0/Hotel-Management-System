3b09cbdb319a8bfe09d0f4b682976ff495e5fc83
Abhi-ram-0 <abhichowdary.0458@gmail.com>
2025-03-10 14:06:43 +0530

    Create Guest.java

---
+++ src/Guest.java
@@ -0,0 +1,22 @@
+public class Guest {
+    private String name;
+    private String contact;
+
+    public Guest(String name, String contact) {
+        this.name = name;
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    @Override
+    public String toString() {
+        return "Guest: " + name + " | Contact: " + contact;
+    }
+}
