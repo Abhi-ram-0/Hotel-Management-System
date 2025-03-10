e8e4e21d1ed9fe39f1bf4bb3f7c3d6e5ed06b42f
Abhi-ram-0 <abhichowdary.0458@gmail.com>
2025-03-10 14:07:19 +0530

    Create Reservation.java

---
+++ src/Reservation.java
@@ -0,0 +1,34 @@
+public class Reservation {
+    private Room room;
+    private Guest guest;
+    private String checkInDate;
+    private String checkOutDate;
+
+    public Reservation(Room room, Guest guest, String checkInDate, String checkOutDate) {
+        this.room = room;
+        this.guest = guest;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public String getCheckInDate() {
+        return checkInDate;
+    }
+
+    public String getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    @Override
+    public String toString() {
+        return room.toString() + "\n" + guest.toString() + "\nCheck-In: " + checkInDate + " | Check-Out: " + checkOutDate;
+    }
+}
